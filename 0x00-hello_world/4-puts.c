3abfb4ee10a854539d373f66d5f436a188e48ea7
Benalla-Aiman <benallaaiman@gmailcom>
2023-03-16 18:31:57 -0700

    the task number 4 of 0x00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - a program that will prints a line with puts function
+ * 
+ * Return: 0 (Success)
+ */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle\n");
