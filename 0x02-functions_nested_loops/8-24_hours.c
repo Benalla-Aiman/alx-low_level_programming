60a56192104629960ce1b05b7b48c01793be38ad
Benalla-Aiman <benallaaiman@gmailcom>
2023-03-23 23:38:22 -0700

    the task number 8 of 0x02

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,6 @@
-#include main.h
+#include <stdio.h>   /* for putchar function */
+#include <stdlib.h>  /* for implicit declaration of _putchar function */
+#include "main.h"    /* assuming that main.h contains other declarations */
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
@@ -6,23 +8,23 @@
  */
 void jack_bauer(void)
 {
-int i, j;
+    int i, j;
 
-i = 0;
+    i = 0;
 
-while (i < 24)
-{
-j = 0;
-while (j < 60)
-{
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-_putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-_putchar('\n');
-j++;
-}
-i++;
-}
+    while (i < 24)
+    {
+        j = 0;
+        while (j < 60)
+        {
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
+            putchar(':');
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
+            putchar('\n');
+            j++;
+        }
+        i++;
+    }
 }
