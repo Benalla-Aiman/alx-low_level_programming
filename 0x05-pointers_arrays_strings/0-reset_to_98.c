80fdf491ccd43afd6e724a26dd3bf0c4a23b73a1
Benalla-Aiman <benallaaiman@gmailcom>
2023-03-28 04:03:48 -0700

    the task number

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 /**
- * reset_to_98 - updates the value of an int to 98
- * @n: pointer to the int to update
- *
- * Return: void
- */
+* reset_to_98 - updates the value of an int to 98
+* @n: pointer to the int to update
+*
+* Return: void
+*/
 void reset_to_98(int *n)
 {
-    *n = 98;
+*n = 98;
 }
