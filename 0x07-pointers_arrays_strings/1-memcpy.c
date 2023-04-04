5a4fc36e399a29473abe3bd31435250aa191dd37
Benalla-Aiman <benallaaiman@gmailcom>
2023-04-04 10:58:36 -0700

    the second task of 0x07 pointers arrays strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,18 @@
 /**
- * _memcpy - copies n bytes from memory area src to memory area dest
- * @dest: pointer to destination
- * @src: pointer to source
- * @n: number of bytes to copy
- *
- * Return: pointer to destination
- */
+* _memcpy - copies n bytes from memory area src to memory area dest
+* @dest: pointer to destination
+* @src: pointer to source
+* @n: number of bytes to copy
+*
+* Return: pointer to destination
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        char *pdest = dest;
-        char *psrc = src;
-
-        while (n--)
-        {
-                *pdest++ = *psrc++;
-        }
-
-        return (dest);
+char *pdest = dest;
+char *psrc = src;
+while (n--)
+{
+*pdest++ = *psrc++;
+}
+return (dest);
 }
