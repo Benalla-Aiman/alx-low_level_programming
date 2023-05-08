64ca4d9ccc7589368662b29cf7208479b628e34d
Benalla-Aiman <benallaaiman@gmailcom>
2023-05-08 13:19:46 -0700

    the is the task number 5 of the inputoutput fileio

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,5 +5,10 @@
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+void check_argc(int argc);
+void open_files(char **argv, int *fd_from, int *fd_to);
+void copy_file(int fd_from, int fd_to, char **argv);
+void close_files(int fd_from, int fd_to);
+int main(int argc, char **argv);
 
 #endif /* MAIN_H */
